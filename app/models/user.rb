e78d5d3976dbc781cb1c2c1e3d9e551fd326d98c
Ryan Wold <ryan.wold@gsa.gov>
2020-11-19 21:31:44 -0800

    remove unused methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -123,13 +123,6 @@ class User < ApplicationRecord
     end
   end
 
-  def set_api_key
-    update(api_key: ApiKey.generator, api_key_updated_at: Time.now)
-  end
-
-  def unset_api_key
-    update(api_key: nil, api_key_updated_at: nil)
-  end
 
   private
 
